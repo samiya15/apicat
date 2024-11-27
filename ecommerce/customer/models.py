6ae8241b0a99374b95855cc0dbc5631d5663a354
Samiya Issack <samiya.abdullahi@strathmore.edu>
2024-11-27 13:08:45 +0300

    add customer and order models

--- ecommerce/customer/models.py
+++ ecommerce/customer/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class customer(models.Model):
+    first_name=models.CharField(max_lengths=50)
+    last_name=models.CharField(max_length=50)
+    email=models.EmailField(unique=True)
+
+def __str__(self):
+    return self.first_name +" "+self.last_name
--- ecommerce/order/models.py
+++ ecommerce/order/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from customer.models import customer
 # Create your models here.
+
+class Order(models.Model):
+    customer=models.ForeignKey(customer,on_delete=models.CASCADE, related_name='orders')
+    order_date=models.DateTimeField(auto_now_add=True)
+    order_total=models.DecimalField(max_digits=10,decimal_places=2)
+    
+    def _str_(self):
+        return self.customer.first_name +" " +self.customer.last_name +" "+str(self.order_total)
+    
